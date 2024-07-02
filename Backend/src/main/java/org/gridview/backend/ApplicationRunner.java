46a8b24e3345ffd5ea7332d674c439c7c4840474
= <=>
2024-07-02 16:51:17 +0530

    Create project

---
+++ Backend/src/main/java/org/gridview/backend/ApplicationRunner.java
@@ -0,0 +1,10 @@
+package org.gridview.backend;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationRunner {
+    public static void main(String[] args) {
+
+    }
+}
